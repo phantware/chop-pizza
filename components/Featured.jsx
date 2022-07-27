cfce29ebceac902aa62af930a37603b22589f8ee
phantware <phantmoney2011@gmail.com>
2022-07-27 11:48:17 +0100

    slidder created

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -1,28 +1,50 @@
 import styles from '../styles/Featured.module.css'
 import Image from 'next/image'
+import { useState } from 'react'
 
 const Featured = () => {
+  const [index, setIndex] = useState(0)
   const images = [
-    '/img/featured1.jpg',
-    '/img/featured2.jpg',
-    '/img/featured3.jpg',
-    '/img/featured4.jpg',
-    '/img/featured5.jpg',
+    '/img/italian.png',
+    '/img/pizza-2.png',
+    '/img/eat-2.png',
+    '/img/featured4-2.png',
   ]
+
+  const handleArrow = (direction) => {
+    if (direction === 'l') {
+      setIndex(index !== 0 ? index - 1 : 3)
+    }
+    if (direction === 'r') {
+      setIndex(index !== 3 ? index + 1 : 0)
+    }
+  }
+
   return (
     <div className={styles.container}>
-      <div className={styles.arrowContainer} style={{ left: 0 }}>
-        <Image src='/img/arrowl.png' layout='fill' objectFit='contain' alt='' />
+      <div
+        className={styles.arrowContainer}
+        style={{ left: 0 }}
+        onClick={() => handleArrow('l')}
+      >
+        <Image src='/img/arrowl.png' alt='' layout='fill' objectFit='contain' />
       </div>
-      <div className={styles.wrapper}>
-        {images.map((image, i) => (
-          <div key={i} className={styles.imgContainer}>
-            <Image src={image} layout='fill' alt='' />
+      <div
+        className={styles.wrapper}
+        style={{ transform: `translateX(${-100 * index}vw)` }}
+      >
+        {images.map((img, i) => (
+          <div className={styles.imgContainer} key={i}>
+            <Image src={img} alt='' layout='fill' objectFit='contain' />
           </div>
         ))}
       </div>
-      <div className={styles.arrowContainer} style={{ right: 0 }}>
-        <Image src='/img/arrowr.png' layout='fill' alt='' />
+      <div
+        className={styles.arrowContainer}
+        style={{ right: 0 }}
+        onClick={() => handleArrow('r')}
+      >
+        <Image src='/img/arrowr.png' layout='fill' alt='' objectFit='contain' />
       </div>
     </div>
   )
