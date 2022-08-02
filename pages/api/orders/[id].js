f4c0886be733c798e90c34fb75baeaa29c2965c7
phantware <phantmoney2011@gmail.com>
2022-08-02 10:17:13 +0100

    order api logic created

--- pages/api/orders/[id].js
+++ pages/api/orders/[id].js
@@ -1,20 +1,35 @@
 import dbConnect from '../../../util/mongo'
 import Product from '../../../models/Product'
+import Order from '../../../models/Order'
+
 export default async function handler(req, res) {
-  const { method } = req
+  const {
+    method,
+    query: { id },
+    cookies,
+  } = req
 
   await dbConnect()
 
   if (method === 'GET') {
     try {
-      const products = await Product.find()
-      res.status(200).json(products)
+      const product = await Product.findById(id)
+      res.status(200).json(product)
     } catch (err) {
       res.status(500).json(err)
     }
   }
 
-  if (method === 'POST') {
+  if (method === 'PUT') {
+    try {
+      const product = await Product.create(req.body)
+      res.status(201).json(product)
+    } catch (err) {
+      res.status(500).json(err)
+    }
+  }
+
+  if (method === 'DELETE') {
     try {
       const product = await Product.create(req.body)
       res.status(201).json(product)
--- pages/api/orders/index.js
+++ pages/api/orders/index.js
@@ -1,5 +1,6 @@
+import Order from '../../../models/Order'
 import dbConnect from '../../../util/mongo'
-import Product from '../../../models/Product'
+
 export default async function handler(req, res) {
   const { method } = req
 
@@ -7,8 +8,8 @@ export default async function handler(req, res) {
 
   if (method === 'GET') {
     try {
-      const products = await Product.find()
-      res.status(200).json(products)
+      const orders = await Order.find()
+      res.status(200).json(orders)
     } catch (err) {
       res.status(500).json(err)
     }
@@ -16,8 +17,8 @@ export default async function handler(req, res) {
 
   if (method === 'POST') {
     try {
-      const product = await Product.create(req.body)
-      res.status(201).json(product)
+      const order = await Order.create(req.body)
+      res.status(201).json(order)
     } catch (err) {
       res.status(500).json(err)
     }
