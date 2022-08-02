a21cda5a7508db13420831b88f7b93ab3eff3d24
phantware <phantmoney2011@gmail.com>
2022-08-02 06:39:40 +0100

    link to cart implemented

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -30,12 +30,14 @@ const Navbar = () => {
           <li className={styles.listItem}>Contact</li>
         </ul>
       </div>
-      <div className={styles.item}>
-        <div className={styles.cart}>
-          <Image src='/img/cart.png' alt='' width='30px' height='30px' />
-          <div className={styles.counter}>{quantity}</div>
+      <Link href='/cart'>
+        <div className={styles.item}>
+          <div className={styles.cart}>
+            <Image src='/img/cart.png' alt='' width='30px' height='30px' />
+            <div className={styles.counter}>{quantity}</div>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   )
 }
