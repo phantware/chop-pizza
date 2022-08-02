213db77ea1e13c004ad031cdb4a51a5e53d37a47
phantware <phantmoney2011@gmail.com>
2022-08-02 13:35:28 +0100

    payment re-route to order pending page

--- pages/cart.js
+++ pages/cart.js
@@ -1,5 +1,8 @@
 import styles from '../styles/Cart.module.css'
 import Image from 'next/image'
+import axios from 'axios'
+import { useRouter } from 'next/router'
+import { reset } from '../redux/cartSlice'
 import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import {
@@ -9,13 +12,24 @@ import {
 } from '@paypal/react-paypal-js'
 
 const Cart = () => {
-  const [loader, setLoader] = useState(true)
-  const dispatch = useDispatch()
   const cart = useSelector((state) => state.cart)
+  const [loader, setLoader] = useState(true)
   const [open, setOpen] = useState(false)
+  const dispatch = useDispatch()
+  const router = useRouter()
+
+  const createOrder = async (data) => {
+    try {
+      const res = await axios.post('http://localhost:3000/api/orders', data)
+      res.status === 201 && router.push('/orders/' + res.data._id)
+      dispatch(reset())
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
   // This values are the props in the UI
-  const amount = '2'
+  const amount = cart.total
   const currency = 'USD'
   const style = { layout: 'vertical' }
 
@@ -62,8 +76,13 @@ const Cart = () => {
           }}
           onApprove={function (data, actions) {
             return actions.order.capture().then(function (details) {
-              console.log('details', details)
-              // Your code here after capture the order
+              const shipping = details.purchase_units[0].shipping
+              createOrder({
+                customer: shipping.name.full_name,
+                address: shipping.address.address_line_1,
+                total: cart.total,
+                method: 1,
+              })
             })
           }}
         />
@@ -79,52 +98,56 @@ const Cart = () => {
       <div className={styles.container}>
         <div className={styles.left}>
           <table className={styles.table}>
-            <tr className={styles.trTitle}>
-              <th>Product</th>
-              <th>Name</th>
-              <th>Extras</th>
-              <th>Price</th>
-              <th>Quantity</th>
-              <th>Total</th>
-            </tr>
+            <tbody>
+              <tr className={styles.trTitle}>
+                <th>Product</th>
+                <th>Name</th>
+                <th>Extras</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Total</th>
+              </tr>
+            </tbody>
 
             {/* Map cart */}
-            {cart.products.map((product) => (
-              <tr className={styles.tr} key={product._id}>
-                <td>
-                  <div className={styles.imgContainer}>
-                    <Image
-                      src={product.img}
-                      layout='fill'
-                      objectFit='cover'
-                      alt=''
-                    />
-                  </div>
-                </td>
-                <td>
-                  <span className={styles.name}>{product.title}</span>
-                </td>
-                <td>
-                  <span className={styles.extras}>
-                    {/* Map Extras */}
-                    {product.extras.map((extra) => (
-                      <span key={extra._id}>{extra.text}, </span>
-                    ))}
-                  </span>
-                </td>
-                <td>
-                  <span className={styles.price}>${product.price}</span>
-                </td>
-                <td>
-                  <span className={styles.quantity}>{product.quantity}</span>
-                </td>
-                <td>
-                  <span className={styles.total}>
-                    ${product.price * product.quantity}
-                  </span>
-                </td>
-              </tr>
-            ))}
+            <tbody>
+              {cart.products.map((product) => (
+                <tr className={styles.tr} key={product._id}>
+                  <td>
+                    <div className={styles.imgContainer}>
+                      <Image
+                        src={product.img}
+                        layout='fill'
+                        objectFit='cover'
+                        alt=''
+                      />
+                    </div>
+                  </td>
+                  <td>
+                    <span className={styles.name}>{product.title}</span>
+                  </td>
+                  <td>
+                    <span className={styles.extras}>
+                      {/* Map Extras */}
+                      {product.extras.map((extra) => (
+                        <span key={extra._id}>{extra.text}, </span>
+                      ))}
+                    </span>
+                  </td>
+                  <td>
+                    <span className={styles.price}>${product.price}</span>
+                  </td>
+                  <td>
+                    <span className={styles.quantity}>{product.quantity}</span>
+                  </td>
+                  <td>
+                    <span className={styles.total}>
+                      ${product.price * product.quantity}
+                    </span>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
           </table>
         </div>
         <div className={styles.right}>
